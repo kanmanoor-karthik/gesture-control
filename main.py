bb2eb0a41896e6aff15f2642706d82c626f9f952
kanmanoor-karthik <kanmanoor.karthik@gmail.com>
2026-09-06 19:22:11 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,75 @@
+import cv2
+import mediapipe as mp
+import pyautogui
+import time
+import math
+
+mp_hands=mp.solutions.hands
+mp_drawing=mp.solutions.drawing_utils
+hands=mp_hands.Hands(max_num_hands=1,min_detection_confidence=0.7)
+
+screen_w,screen_h=pyautogui.size()
+capture=cv2.VideoCapture(0)
+prev_screen_x=0
+prev_screen_y=0
+
+click_start_time=None
+click_times=[]
+click_cooldown=0.5
+scroll_cooldown=False
+freeze_cursor=False
+
+if not capture.isOpened():
+    print("cannot open camera")
+    exit()
+while True:
+    bol,frame=capture.read()
+    if not bol:
+        print("can't recive frame")
+        break
+    frame=cv2.flip(frame,1)
+    rgb=cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+    results=hands.process(rgb)
+    if results.multi_hand_landmarks:
+        for hand_landmarks in results.multi_hand_landmarks:
+            mp_drawing.draw_landmarks(frame,hand_landmarks,mp_hands.HAND_CONNECTIONS)
+
+        thumb_tip=hand_landmarks.landmark[4]
+        index_tip=hand_landmarks.landmark[8]
+        middle_tip=hand_landmarks.landmark[12]
+        ring_tip=hand_landmarks.landmark[16]
+        pinky_tip=hand_landmarks.landmark[20]
+
+        fingers=[
+            1 if hand_landmarks.landmark[tip].y<hand_landmarks.landmark[tip-2].y else 0
+            for tip in [8,12,16,20]
+        ]
+        dist=math.hypot(thumb_tip.x-index_tip.x,thumb_tip.y-index_tip.y)
+        if dist<0.08:
+            if not freeze_cursor:
+                freezr_cursor=True
+                click_times.append(time.time())
+
+                if len(click_times)>=2 and click_times[-1]-click_times[-2]<0.4:
+                    pyautogui.doubleClick()
+                    cv2.putText(frame,"Double click",(10,50),cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,255),2)
+                    click_times=[]
+                else:
+                    pyautogui.click()
+                    cv2.putText(frame,"single click",(10,50),cv2.FONT_HERSHEY_SIMPLEX,1,(255,255,0),2)
+            else:
+                if freezr_cursor:
+                    time.sleep(0.1)
+            freeze_cursor=False
+
+        if not freeze_cursor:
+            screen_x=int(index_tip.x*screen_w)
+            screen_y=int(index_tip.y*screen_h)
+            pyautogui.moveTo(screen_x,screen_y,duration=0.20)
+            prev_screen_x,prev_screen_y=screen_x,screen_y
+
+    cv2.imshow("live video",frame)
+    if cv2.waitKey(1)==ord('q'):
+        break
+capture.release()
+cv2.destroyAllWindows()
